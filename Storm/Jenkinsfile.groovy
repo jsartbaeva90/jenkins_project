e6cc94e0ec851596800fe0f423e4c74e1e46c00e
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-28 20:12:45 -0500

    jjjh

---
+++ Storm/Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+node{
+    stage("Update jenkins"){
+        properties([parameters([string(defaultValue: '63.35.235.236', description: 'please provide IP', name: 'ENVIR', trim: true)])])
+        sh "echo Parameter added"
+    }
+    stage("Install git"){
+        sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip -y"
+    }
+    stage("Pull Repo"){
+        sh "ssh  ec2-user@${ENVIR} git clone https://github.com/jsartbaeva90/stormpath-flask-sample.git 2> /dev/null"
+    }
+    stage("Install Requirements"){
+        sh "echo Hello"
+    }
+    stage("Pip Install"){
+        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/stormpath-flask-sample/requirements.txt"
+    }
+    stage("Run App"){
+        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/stormpath-flask-sample/bootstrap.py"
+    }
+}
