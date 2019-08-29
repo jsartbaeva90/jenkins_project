cda6699c2ab549e0380e4039f0a5289f7ff57657
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-28 19:55:32 -0500

    jhghj

---
+++ Hello_world/Jenkinsfile.groovy
@@ -0,0 +1,25 @@
+node{
+    stage("Update jenkins"){
+        properties([parameters([string(defaultValue: '63.35.235.236', description: 'please provide IP', name: 'ENVIR', trim: true)])])        sh "echo Parameter added"
+    }
+    stage("Install git"){
+        sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip -y"
+    }
+    stage("Remove repo"){
+        sh "ssh  ec2-user@${ENVIR} sudo  rm -rf /home/ec2-user/flask-examples"
+    }
+    stage("Pull Repo"){
+        sh "ssh  ec2-user@${ENVIR} git clone  https://github.com/jsartbaeva90/flask-examples.git"
+    }
+    stage("Install Requirements"){
+        //sh "virtualenv /tmp/venv"
+        //sh ". /tmp/venv/bin/activate"
+        sh "echo Hello"
+    }
+    stage("Pip Install"){
+        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/flask-examples/requirements.txt"
+    }
+    stage("Run App"){
+        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/flask-examples/01-hello-world/hello.py"
+    }
+}
